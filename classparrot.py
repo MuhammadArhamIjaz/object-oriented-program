2d2cabbc80f5565f1356bffb3aee0505c9fb6f78
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2025-09-22 20:20:40 +0700

    Add files via upload

---
+++ classparrot.py
@@ -0,0 +1,11 @@
+class parrot:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.species = "bird"
+blu= parrot("blu", 10)
+woo=parrot("Woo",15)
+print("blu is a {}".format(blu.species))
+print("woo is a {}".format(woo.species))
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
