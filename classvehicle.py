5258d489ce2773d6f70d244fa9d62d5382b1ebed
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2025-09-22 20:06:37 +0700

    Add files via upload

---
+++ classvehicle.py
@@ -0,0 +1,9 @@
+class vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelx=vehicle(240,18)
+print(modelx.max_speed)
+print(modelx.mileage)
+print("model maximam speed is ",modelx.max_speed,"kmph")
+print("model mileage is ",modelx.mileage,"kmph")
---
+++ studentclass.py
@@ -0,0 +1,4 @@
+class student:
+    grade=10
+    print("I am a studen of grade",grade)
+ob=student()    
