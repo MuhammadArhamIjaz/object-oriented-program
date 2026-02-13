10bf57cf915d9685b67fb4e8f66fb305838be6f7
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2026-02-13 14:36:27 +0500

    Add files via upload

---
+++ overloading opereator.py
@@ -0,0 +1,21 @@
+class A:
+    def __init__(self,a):
+     self.a=a
+    def __lt__(self,other):
+       if(self.a<other.a):
+          return "ob1 is less than ob2"
+       else:
+            return"ob2 is less than ob1"
+    def __eq__(self,other):
+       if(self.a==other.a):
+          return"Both are equal"
+       else:
+            return"Not equal"
+ob1=A(5)
+ob2=A(3)
+print("Passes Values:",ob1.a, ob2.a)
+print(ob1 < ob2)
+ob3=A(7)
+ob4=A(4)
+print("Passes Values:",ob3.a, ob4.a)
+print(ob3==ob4)
